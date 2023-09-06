ab8c0d195eab60b61282e939a4956ed815c97760
nayeong lee <lny021102@gmail.com>
2023-09-06 12:45:59 +0900

    [ADD] 필요한 모든 scene 추가 및 전환 버튼 추가하여 전반적인 흐름을 구성해둠

---
+++ Assets/Scenes/GoToARCam.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToARCam : MonoBehaviour
+{
+    public void Move()
+    {
+        SceneManager.LoadScene("ARCam"); //¿Ãµø«“ æ¿
+    }
+}
