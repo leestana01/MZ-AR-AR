9172b7668438f57f79d9d775241697cb6ef24ed5
nayeong lee <lny021102@gmail.com>
2023-09-08 14:45:54 +0900

    [ADD] 박스 터뜨리는 부분 UI 수정, 박스 클릭 시 크기 변화되도록 추가

---
+++ Assets/Scenes/makeBigAndSmall.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class makeBigAndSmall : MonoBehaviour
+{
+    private Button button;
+    private RectTransform rectTransform;
+    private Vector2 originalSize;
+    private Vector2 targetSize;
+    private float animationDuration = 0.2f;
+    private float currentTime = 0f;
+    private bool isAnimating = false;
+    private bool isButtonClicked = false; //버튼이 클릭됐는가 아닌가?
+
+    public void Start()
+    {
+        //버튼 및 RectTransform 컴포넌트를 가져온다
+        button = GetComponent<Button>();
+        rectTransform = GetComponent<RectTransform>();
+
+        //초기 랜덤박스 버튼의 크기를 저장한다
+        originalSize = rectTransform.sizeDelta;
+        targetSize = originalSize;
+
+        //버튼 클릭 이벤트에 함수를 연결함
+        button.onClick.AddListener(ChangeSize);
+    }
+
+    public void Update()
+    {
+        //만약 애니메이션 중이면 크게 크기 변경
+        if (isAnimating)
+        {
+            currentTime += Time.deltaTime;
+            float t = Mathf.Clamp01(currentTime / animationDuration);
+            rectTransform.sizeDelta = Vector2.Lerp(originalSize, targetSize, t);
+
+            if (t >= 1f)
+            {
+                isAnimating = false;
+                currentTime = 0f;
+            }
+        }
+    }
+
+    public void ChangeSize()
+    {
+
+        if (isAnimating)
+        {
+            return;
+        }
+
+        //버튼 크기 변경
+        if (rectTransform.sizeDelta == originalSize)
+        {
+            //버튼이 클릭되었을 때 크기를 키운다
+            targetSize = originalSize * 1.5f;
+        }
+        else
+        {
+            //버튼이 클릭되지 않은 상태라면 초기 설정한 크기가 된다
+            targetSize = originalSize;
+        }
+
+        isAnimating = true;
+    }
+}
